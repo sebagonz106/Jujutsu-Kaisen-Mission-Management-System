f5733cf40a094e2b7b2166a82e85b12a61a7187c
Sebastian <123786583+sebagonz106@users.noreply.github.com>
2025-12-01 16:43:33 -0500

    [FE] Load frontend for all roles

--- mission_management/src/routes/RoleGuard.tsx
+++ mission_management/src/routes/RoleGuard.tsx
@@ -31,8 +31,8 @@ import React from 'react';
 export const RoleGuard: React.FC<{ roles: Array<'sorcerer' | 'support' | 'admin'>; children: React.ReactNode }> = ({ roles, children }) => {
   const { user } = useAuth();
   if (!user) return <Navigate to="/login" replace />;
-  // Admin has full access
-  if (user.role === 'admin' || user.role == 'support') return <>{children}</>;
-  if (!roles.includes(user.role)) return <Navigate to="/403" replace />;
+  // // Admin has full access
+  // if (user.role === 'admin') return <>{children}</>;
+  // if (!roles.includes(user.role)) return <Navigate to="/403" replace />;
   return <>{children}</>;
 };
